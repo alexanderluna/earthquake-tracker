38b3f4e2ddf1ca7493a204c3acde7e5846771f6d
Mycroft1891 <mycroft1891@gmail.com>
2018-01-26 23:02:44 +0100

    create drop down menu component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,9 @@ import { Component }  from 'react';
 import AppBar         from 'material-ui/AppBar';
 import FlatButton     from 'material-ui/FlatButton';
 import EarthquakeList from './EarthquakeList';
+import UserSelection  from './UserSelection';
 import { getQuakes }  from '../services/earthquakes';
 import { getCity   }  from '../services/earthquakes';
-import SelectField    from 'material-ui/SelectField';
-import MenuItem       from 'material-ui/MenuItem';
 import SearchBar      from 'material-ui-search-bar'
 
 class Home extends Component {
@@ -80,38 +79,15 @@ class Home extends Component {
           hintText="Search City: Tokyo, Berlin..."
         />
 
-        <SelectField
-          className="select-field"
-          floatingLabelText="Radius"
-          value={this.state.radius}
-          onChange={this.handler}
-          autoWidth={true}
-        >
-          <MenuItem value={50} primaryText="50 KM" />
-          <MenuItem value={100} primaryText="100 KM" />
-          <MenuItem value={250} primaryText="250 KM" />
-          <MenuItem value={500} primaryText="500 KM" />
-          <MenuItem value={1000} primaryText="1000 KM" />
-          <MenuItem value={2000} primaryText="2000 KM" />
-          <MenuItem value={4000} primaryText="4000 KM" />
-        </SelectField>
-
-        <SelectField
-          className="select-field"
-          floatingLabelText="Magnitude"
-          value={this.state.magnitude}
-          onChange={this.handler}
-          autoWidth={true}
-        >
-          <MenuItem value={2} primaryText="mag 2+" />
-          <MenuItem value={3} primaryText="mag 3+" />
-          <MenuItem value={4} primaryText="mag 4+" />
-          <MenuItem value={5} primaryText="mag 5+" />
-          <MenuItem value={6} primaryText="mag 6+" />
-          <MenuItem value={7} primaryText="mag 7+" />
-        </SelectField>
-
-        <h2>({this.state.earthquakes.length}) earthquakes near {this.state.city}</h2>
+        <div className="top-section">
+          <UserSelection
+            magnitude={this.state.magnitude}
+            radius={this.state.radius}
+            handler={this.handler}
+          />
+          <h2>({this.state.earthquakes.length}) earthquakes near:</h2>
+          <h2>{this.state.city}</h2>
+        </div>
         <EarthquakeList list={this.state.earthquakes}/>
       </div>
     )
