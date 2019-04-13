4473354574c8a57a5a2a576346156d50492a056d
Mycroft1891 <mycroft1891@gmail.com>
2019-04-13 17:27:25 +0200

    Update: follow eslint style guide

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React      from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
