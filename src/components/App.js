db2acc19e811c0d6c948dff39ef94e5dedc502e1
Mycroft1891 <mycroft1891@gmail.com>
2019-04-14 17:28:43 +0200

    Update: follow eslint style guide and move header to own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,18 @@
-import React, { Component } from 'react';
-import MuiThemeProvider     from 'material-ui/styles/MuiThemeProvider';
-import AppBar               from 'material-ui/AppBar';
-import Home                 from './Home';
-import ShowEarthquake       from './ShowEarthquake';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import Header from './partials/Header';
+import ShowEarthquake from './earthquake/ShowEarthquake';
 import '../App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-        <Router>
-          <div>
-            <AppBar
-              title={ <Link to="/">Earthquaky</Link>}
-              className="app-bar"
-              showMenuIconButton={false}
-            />
-            <Route exact path="/" component={Home}/>
-            <Route path="/quake/:id" component={ShowEarthquake}/>
-          </div>
-        </Router>
-      </MuiThemeProvider>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/quake/:id" component={ShowEarthquake} />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
