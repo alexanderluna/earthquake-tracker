25628a9727729cf596868a914b3fb088f5117cd7
Mycroft1891 <mycroft1891@gmail.com>
2019-04-17 17:31:31 +0200

    Update: clean up and follow eslint style guide

--- src/services/earthquakes.js
+++ src/services/earthquakes.js
@@ -1,19 +1,19 @@
 export function getQuakes(query) {
-  const one_week = 604800000;
+  const oneWeek = 604800000;
   const today = new Date();
-  const one_week_ago = new Date(today - one_week);
-  return fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${one_week_ago.toDateString()}&latitude=${query.lat}&longitude=${query.lon}&maxradiuskm=${query.rad}&minmagnitude=${query.mag}`)
-  .then(res => res.json())
+  const oneWeekAgo = new Date(today - oneWeek);
+  return fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${oneWeekAgo.toDateString()}&latitude=${query.lat}&longitude=${query.lon}&maxradiuskm=${query.rad}&minmagnitude=${query.mag}`)
+    .then(res => res.json());
 }
 
 
 export function getCity(city) {
-  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.REACT_APP_GOOGLE_KEY}`)
-  .then(res => res.json())
-  .then(json => json.results[0])
+  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyChZPizXo_3sk70Cm4yveOd0YfQtuxc7As`)
+    .then(res => res.json())
+    .then(json => json.results[0]);
 }
 
 export function findQuake(id) {
   return fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventid=${id}`)
-  .then(res => res.json())
+    .then(res => res.json());
 }
